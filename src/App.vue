85ea3760c1d123485ac31ae2d0254f654d31a72e
eldar <eldarmutaev@inbox.ru>
2024-03-23 17:51:39 +0300

    header

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header/>
+
+
+    <h1>Агай</h1>
+
+
+  </div>
+</template>
+
+
