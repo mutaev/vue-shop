e60e260603b5042f945f258cbe76968bf8ab57b9
eldar <eldarmutaev@inbox.ru>
2024-03-24 19:23:19 +0300

    cart

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,22 @@
   <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
     <Header/>
 
+    <div class="">
+      <div>
+        <img src="/like-2.svg" alt="Like-1">
+        <img src="/sneakers/sneakers-1.jpg" alt="Sneaker">
+        <p>Мужкие Кроссовки  Nike Blazer Mud Suede</p>
 
-    <h1>Агай</h1>
+        <div class="flex justify-between">
+          <div>
+            <span>Цена:</span>
+            <span>1205 руб.</span>
+          </div>
+
+          <img src="/plus.svg" alt="plus">
+        </div>
+      </div>
+    </div>
 
 
   </div>
