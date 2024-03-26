a386f00bce8d605cb894f1b26ce04a05ab30f870
eldar <eldarmutaev@inbox.ru>
2024-03-26 14:36:40 +0300

    drawer

---
+++ src/components/CartItem.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+  <div class="flex items-center border border-slate-200 p-4 rounded-xl gap-4">
+    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+
+    <div class="flex flex-col">
+      <p>Кросы адидас ахиdasdfgasdfasdfasdfasdfasdf</p>
+
+      <div class="flex justify-between mt-2">
+        <b>12999 руб.</b>
+        <img class="opacity-40 hover:opacity-100 transition cursor-pointer" src="/close.svg">
+      </div>
+    </div>
+  </div>
+</template>
+
+
